baf22b818dc2448eeb71cff0f3f1738b0457370b
Neudo <78046919+Neudo@users.noreply.github.com>
2021-07-03 17:35:15 +0200

    les 2 hovers + horizontal scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,37 @@
+// changement de couleur contact au scroll
 const contact = document.querySelector(".right a");
 
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  console.log(clientHeight);
 
-  if (clientHeight + scrollTop >= scrollHeight - clientHeight - 20) {
+  if (scrollTop > clientHeight - 30) {
     contact.classList.add("black");
     contact.classList.remove("white");
   } else if (clientHeight + scrollTop <= scrollHeight - clientHeight + 20) {
     contact.classList.add("white");
     contact.classList.remove("black");
   }
+});
+
+// horizontal scroll
+
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".container--works .horizontal");
+console.log(sections);
+let allSections = gsap.utils.toArray(".main-wrapper section");
+console.log(allSections);
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container--horizontal",
+    pin: true,
+    start: "top",
+    markers: false,
+    scrub: 1,
+    end: () =>
+      "+=" + document.querySelector(".container--works").offsetWidth / 4,
+  },
 });
